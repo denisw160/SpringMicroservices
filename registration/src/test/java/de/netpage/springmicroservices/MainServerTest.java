a948947818b4f44e65f9695f66a49e44664838b7
Denis Wirries <denis@wirries.me>
2016-07-16 21:57:20 +0200

    Test for Server

---
+++ registration/src/test/java/de/netpage/springmicroservices/MainServerTest.java
@@ -0,0 +1,17 @@
+package de.netpage.springmicroservices;
+
+/**
+ * Testcases for MainServer
+ *
+ * @author Denis Wirries
+ * @version 1.0
+ * @since 16.07.16
+ */
+public class MainServerTest {
+
+    @org.junit.Test
+    public void main() throws Exception {
+        MainServer.main(new String[]{});
+    }
+
+}
