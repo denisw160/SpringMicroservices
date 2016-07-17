ecf4e959356e604cc04903d7a556d3ba81e6ddda
Denis Wirries <denis@wirries.me>
2016-07-17 14:00:42 +0200

    Script

---
+++ registration.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Settings for Service
+SERVICE=registration
+JAR=$SERVICE.jar
+PID=$SERVICE.pid
+LOG=$SERVICE.log
+
+# Setup Environment
+JAVA=java
+
+case $1 in
+    start)
+        echo "Starting $SERVICE ..."
+        if [ ! -f $PID ]; then
+            nohup $JAVA -jar $PATH >> $LOG &
+            echo $! > $PID
+            echo "$SERVICE started."
+        else
+            echo "$SERVICE is already running."
+        fi
+    ;;
+    stop)
+        if [ -f $PID ]; then
+            PID=$(cat $PID);
+            echo "$SERVICE stopping ..."
+            kill $PID;
+            echo "$SERVICE stopped."
+            rm $PID
+        else
+            echo "$SERVICE is not running."
+        fi
+    ;;
+    restart)
+        if [ -f $PID ]; then
+            PID=$(cat $PID);
+            echo "$SERVICE stopping ...";
+            kill $PID;
+            echo "$SERVICE stopped.";
+            rm $PID
+            echo "$SERVICE starting ..."
+            nohup $JAVA -jar $PATH >> $LOG &
+            echo $! > $PID
+            echo "$SERVICE started."
+        else
+            echo "$SERVICE is not running."
+        fi
+    ;;
+esac
